97d0fe26d535c841dabe4cb9d4d842fa669f6896
johnyang70581 <john.yang@stackcore.com>
2022-04-15 18:11:55 +0800

    [build]: add the first vueCalendar.

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Calendar />
+</template>
+<script lang="ts">
+  import { defineComponent } from "vue";
+  import Calendar from "./components/Calendar.vue";
+
+  export default defineComponent({
+    name: "App",
+    components: {
+      Calendar,
+    }
+  });
+</script>
+<style>
+@import "./assets/base.scss";
+@import "./assets/calendar.scss";
+</style>
